db622dbde9df85582f79c263154995da62cded79
Tomas Karasek <tom.to.the.k@gmail.com>
2023-01-13 13:18:04 +0100

    first

---
+++ empy/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from empy.api.devices_api import DevicesApi
+from empy.api.organizations_api import OrganizationsApi
+from empy.api.projects_api import ProjectsApi
